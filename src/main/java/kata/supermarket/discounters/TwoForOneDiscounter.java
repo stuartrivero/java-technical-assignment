fdb75c93dca5af20d2fb6936c5009f15ff99ebc5
Stuart Rivero <stuart.rivero@thinkto.be>
2021-03-18 15:48:56 +0000

    Add TwoForOneDiscounter for a single Product

--- src/main/java/kata/supermarket/discounters/TwoForOneDiscounter.java
+++ src/main/java/kata/supermarket/discounters/TwoForOneDiscounter.java
@@ -3,11 +3,23 @@ package kata.supermarket.discounters;
 import java.math.BigDecimal;
 import java.util.List;
 import kata.supermarket.Item;
+import kata.supermarket.ItemByUnit;
+import kata.supermarket.Product;
 
 public class TwoForOneDiscounter implements Discounter {
 
+    private final Product product;
+
+    public TwoForOneDiscounter(Product product) {
+        this.product = product;
+    }
+
     @Override
     public BigDecimal calculateDiscount(List<Item> items) {
-        return BigDecimal.ONE;
+        long number = items.stream()
+                .filter(ItemByUnit.class::isInstance)
+                .filter(it -> ((ItemByUnit) it).getProduct().equals(product)).count();
+        long pairs = number / 2;
+        return product.oneOf().price().multiply(BigDecimal.valueOf(pairs));
     }
 }
--- src/test/java/kata/supermarket/discounters/TwoForOneDiscounterTest.java
+++ src/test/java/kata/supermarket/discounters/TwoForOneDiscounterTest.java
@@ -1,17 +1,57 @@
 package kata.supermarket.discounters;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import kata.supermarket.Item;
+import kata.supermarket.Product;
+import kata.supermarket.WeighedProduct;
 import org.junit.jupiter.api.Test;
 
 class TwoForOneDiscounterTest {
 
     @Test
-    void fixedDiscount() {
-        BigDecimal discount = new TwoForOneDiscounter().calculateDiscount(Collections.emptyList());
-        assertEquals(BigDecimal.ONE, discount);
+    void packetsOfBiscuitsAreTwoForOne() {
+        BigDecimal pricePerUnit = new BigDecimal("1.99");
+        Product biscuits = new Product(pricePerUnit);
+
+        List<Item> items = new ArrayList<>();
+        items.add(biscuits.oneOf());
+        items.add(biscuits.oneOf());
+
+        BigDecimal discount = new TwoForOneDiscounter(biscuits).calculateDiscount(items);
+        assertEquals(pricePerUnit, discount);
+    }
+
+    @Test
+    void forThreePacketOfBiscuitsTwoAreCharged() {
+        BigDecimal pricePerUnit = new BigDecimal("1.99");
+        Product biscuits = new Product(pricePerUnit);
+
+        List<Item> items = new ArrayList<>();
+        items.add(biscuits.oneOf());
+        items.add(biscuits.oneOf());
+        items.add(biscuits.oneOf());
+
+        BigDecimal discount = new TwoForOneDiscounter(biscuits).calculateDiscount(items);
+        assertEquals(pricePerUnit, discount);
+    }
+
+    @Test
+    void forTwoPacketOfBiscuitsAndAChocolateBarTheBiscuitsAreDiscounted() {
+        BigDecimal pricePerUnitBiscuits = new BigDecimal("1.99");
+        BigDecimal pricePerUnitChocBar = new BigDecimal("0.95");
+        Product biscuits = new Product(pricePerUnitBiscuits);
+        Product chocBar = new Product(pricePerUnitChocBar);
+
+        List<Item> items = new ArrayList<>();
+        items.add(biscuits.oneOf());
+        items.add(biscuits.oneOf());
+        items.add(chocBar.oneOf());
+
+        BigDecimal discount = new TwoForOneDiscounter(biscuits).calculateDiscount(items);
+        assertEquals(pricePerUnitBiscuits, discount);
     }
 }
