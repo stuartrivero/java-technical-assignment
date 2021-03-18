7de92df88c9e871ad8af8da4c31dba0fe64f2bff
Stuart Rivero <stuart.rivero@thinkto.be>
2021-03-18 12:47:08 +0000

    Added Discounter to basket

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -8,8 +8,10 @@ import java.util.List;
 
 public class Basket {
     private final List<Item> items;
+    private final Discounter discounter;
 
-    public Basket() {
+    public Basket(Discounter discounter) {
+        this.discounter = discounter;
         this.items = new ArrayList<>();
     }
 
@@ -39,15 +41,8 @@ public class Basket {
                     .setScale(2, RoundingMode.HALF_UP);
         }
 
-        /**
-         * TODO: This could be a good place to apply the results of
-         *  the discount calculations.
-         *  It is not likely to be the best place to do those calculations.
-         *  Think about how Basket could interact with something
-         *  which provides that functionality.
-         */
         private BigDecimal discounts() {
-            return BigDecimal.ZERO;
+            return discounter.calculateDiscount(items);
         }
 
         private BigDecimal calculate() {
---
+++ src/main/java/kata/supermarket/Discounter.java
@@ -0,0 +1,11 @@
+package kata.supermarket;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Discounter {
+
+    public BigDecimal calculateDiscount(List<Item> items) {
+        return BigDecimal.ONE;
+    }
+}
